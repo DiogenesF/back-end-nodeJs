d683f413337f2ef7bcc5295b0a3c98f67e92d23e
Diogenes Fiorezi <diogenesfiorezi@hotmail.com>
2020-01-05 01:00:52 -0300

    Node callbacks and error handling

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ var rect = require("./rectangle");
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("The dimensions must be > than 0!");
-    } else {
-        console.log("The area is: " + rect.area(l,b));
-        console.log("The perimeter is: " + rect.perimeter(l,b));
-    }
+    rect(l,b, (err, rectangle) => {
+        if (err) {
+            console.log("ERROR: ", err.message);
+        }
+        else {
+            console.log("The area of the rectangle is: " + rectangle.area());
+            console.log("The perimeter of the rectangle is: " + rectangle.perimeter());
+        }
+    });
+    console.log("After the call!");
 }
 
 solveRect(2,4);
