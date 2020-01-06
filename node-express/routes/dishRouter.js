5b6f69a442d1c15b49297edf339e3b15310e5f91
Diogenes Fiorezi <diogenesfiorezi@hotmail.com>
2020-01-06 00:01:52 -0300

    Assignment 1

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -25,4 +25,26 @@ dishRouter.route("/")
     res.end("Deleting all the dishes!");
 });
 
+dishRouter.route("/:dishId")
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next()
+})
+.get((req,res,next) => {
+    res.end("Will send dish " + req.params.dishId + " to you!");
+})
+.post((req,res,next) => {
+    res.statusCode = 403;
+    res.end("POST operations not supported on /dishes/" + req.params.dishId);
+})
+.put((req,res,next) => {
+    res.write("You are updating dish " + req.params.dishId + "\n");
+    res.end("Will update the dish to: " + req.body.name + " and description to: " + req.body.description);
+})
+.delete((req,res,next) => {
+    res.end("Deleting the dish " + req.params.dishId);
+});
+
+
 module.exports = dishRouter;
