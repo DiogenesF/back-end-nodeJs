8a5b333ced01792b111e71731d054ad6151488ab
Diogenes Fiorezi <diogenesfiorezi@hotmail.com>
2020-02-08 03:14:54 -0300

    Passport facebook OAuth

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    "secretKey": "12345-67890-09876-54321",
-    "mongoUrl": "mongodb://localhost:27017/confusion"
-}
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/confusion",
+  facebook: {
+    clientId: "184974519233825",
+    clientSecret: "a07ba0e0028c1a58ca81b8dfe9ad8847"
+  }
+};
